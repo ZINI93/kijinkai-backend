bc78cbb4702f552715875f92377a8e7f4f16ba1d
ZINI93 <qwas566@gmail.com>
2025-04-20 15:24:27 +0900

    start

---
+++ src/main/java/com/kijinkai/KijinkaiApplication.java
@@ -0,0 +1,13 @@
+package com.kijinkai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KijinkaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KijinkaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kijinkai/KijinkaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kijinkai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KijinkaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
