357c1d6197983ed35080689e60ed049953433940
ZINI93 <qwas566@gmail.com>
2026-01-21 16:45:34 +0900

    feat: 코드 추가

--- src/main/java/com/kijinkai/util/BusinessCodeType.java
+++ src/main/java/com/kijinkai/util/BusinessCodeType.java
@@ -3,7 +3,9 @@ package com.kijinkai.util;
 public enum BusinessCodeType {
 
     ORI("ORI", "주문 상품 식별번호" ),
-    ORD("ORD","주문 식별 번호" ); // 주문코드
+    ORD("ORD","주문 식별 번호" ),
+    BOX("BOX", "배송하는 박스 식별번호"),
+    OP("OP", "주문 결제 식별번호");
 
     private final String prefix;
     private final String description;
