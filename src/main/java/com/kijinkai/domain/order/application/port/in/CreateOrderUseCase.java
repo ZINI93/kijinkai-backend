16449a7f44bd7404beb69857977ecf0071856df8
ZINI93 <qwas566@gmail.com>
2026-01-28 08:12:40 +0900

    refactor: 트랜잭션 구조개선

--- src/main/java/com/kijinkai/domain/order/application/port/in/CreateOrderUseCase.java
+++ src/main/java/com/kijinkai/domain/order/application/port/in/CreateOrderUseCase.java
@@ -2,9 +2,14 @@ package com.kijinkai.domain.order.application.port.in;
 
 import com.kijinkai.domain.order.application.dto.OrderRequestDto;
 import com.kijinkai.domain.order.application.dto.OrderResponseDto;
+import com.kijinkai.domain.orderitem.domain.model.OrderItem;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public interface CreateOrderUseCase {
     OrderResponseDto createOrder(UUID userUuid, OrderRequestDto requestDto);
+    OrderResponseDto createAndSaveOrder(UUID customerUuid, List<OrderItem> orderItems, Map<String,Boolean> inspectedPhotoRequest, String orderCode, BigDecimal totalPrice);
 }
--- src/main/java/com/kijinkai/domain/payment/application/port/in/orderPayment/CreateOrderPaymentUseCase.java
+++ src/main/java/com/kijinkai/domain/payment/application/port/in/orderPayment/CreateOrderPaymentUseCase.java
@@ -1,12 +1,16 @@
 package com.kijinkai.domain.payment.application.port.in.orderPayment;
 
+import com.kijinkai.domain.customer.domain.model.Customer;
 import com.kijinkai.domain.payment.application.dto.request.OrderPaymentDeliveryRequestDto;
 import com.kijinkai.domain.payment.application.dto.request.OrderPaymentRequestDto;
 import com.kijinkai.domain.payment.application.dto.response.OrderPaymentResponseDto;
+import com.kijinkai.domain.payment.domain.model.OrderPayment;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public interface CreateOrderPaymentUseCase {
     OrderPaymentResponseDto createSecondPayment(UUID userUuid, OrderPaymentRequestDto requestDto);
     OrderPaymentResponseDto paymentDeliverFee(UUID userUuid, OrderPaymentDeliveryRequestDto requestDto);
+    OrderPayment saveOrderItem(Customer customer, BigDecimal exchangedAmount);
 }
