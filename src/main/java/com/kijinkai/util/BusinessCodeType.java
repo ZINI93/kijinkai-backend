3b56e9456038cca858e825e8658633c930ab986d
ZINI93 <qwas566@gmail.com>
2026-01-28 08:11:22 +0900

    feat: 식별코드 추가

--- src/main/java/com/kijinkai/util/BusinessCodeType.java
+++ src/main/java/com/kijinkai/util/BusinessCodeType.java
@@ -5,7 +5,9 @@ public enum BusinessCodeType {
     ORI("ORI", "주문 상품 식별번호" ),
     ORD("ORD","주문 식별 번호" ),
     BOX("BOX", "배송하는 박스 식별번호"),
-    OP("OP", "주문 결제 식별번호");
+    ORP("OP", "주문 결제 식별번호"),
+    DEP("DEP", "입금 식별번호"),
+    WIT("WIT", "출금 식별번호");
 
     private final String prefix;
     private final String description;
